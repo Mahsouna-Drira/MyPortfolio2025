5c9c4a8896cc6ba41227e9066f9c0c7d50d89a38
ilyesarous <ilyesarous9645@gmail.com>
2025-10-29 00:34:41 +0100

    fixed the performance

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import React from "react";
 import { Analytics } from "@vercel/analytics/react";
-import About from "./components/about/About";
-import Certifications from "./components/certifications/Certifications";
-import Contact from "./components/contact/Contact";
-import Experience from "./components/experience/Experience";
-import Footer from "./components/footer/Footer";
-import Home from "./components/home/Home";
-import NavBar from "./components/navbar/NavBar";
-import Projects from "./components/projects/Projects";
-import Skills from "./components/skills/Skills";
+
+const NavBar = React.lazy(() => import("./components/navbar/NavBar"));
+const Home = React.lazy(() => import("./components/home/Home"));
+const About = React.lazy(() => import("./components/about/About"));
+const Skills = React.lazy(() => import("./components/skills/Skills"));
+const Certifications = React.lazy(() => import("./components/certifications/Certifications"));
+const Experience = React.lazy(() => import("./components/experience/Experience"));
+const Projects = React.lazy(() => import("./components/projects/Projects"));
+const Contact = React.lazy(() => import("./components/contact/Contact"));
+const Footer = React.lazy(() => import("./components/footer/Footer"));
 
 
 function App() {
