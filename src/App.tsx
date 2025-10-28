7ffdd033d5fea54ed798d8720f3806b4a25e45af
ilyesarous <ilyesarous9645@gmail.com>
2025-10-29 00:47:58 +0100

    fixed the load lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,32 @@
 import React from "react";
 import { Analytics } from "@vercel/analytics/react";
-
-const NavBar = React.lazy(() => import("./components/navbar/NavBar"));
+import Footer from "./components/footer/Footer";
+import NavBar from "./components/navbar/NavBar";
 const Home = React.lazy(() => import("./components/home/Home"));
 const About = React.lazy(() => import("./components/about/About"));
 const Skills = React.lazy(() => import("./components/skills/Skills"));
-const Certifications = React.lazy(() => import("./components/certifications/Certifications"));
-const Experience = React.lazy(() => import("./components/experience/Experience"));
+const Certifications = React.lazy(
+  () => import("./components/certifications/Certifications")
+);
+const Experience = React.lazy(
+  () => import("./components/experience/Experience")
+);
 const Projects = React.lazy(() => import("./components/projects/Projects"));
 const Contact = React.lazy(() => import("./components/contact/Contact"));
-const Footer = React.lazy(() => import("./components/footer/Footer"));
-
 
 function App() {
   return (
     <>
       <NavBar />
       <Home />
-      <About/>
+      <About />
       <Skills />
-      <Certifications/>
-      <Experience/>
+      <Certifications />
+      <Experience />
       <Projects />
-      <Contact/>
+      <Contact />
       <Footer />
-      <Analytics/>
+      <Analytics />
     </>
   );
 }
